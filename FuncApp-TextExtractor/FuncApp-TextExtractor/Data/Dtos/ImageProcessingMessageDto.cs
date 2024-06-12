c6a658349e5d09570038d4200858a8ecff9b1b39
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-12 11:49:03 +0530

    Update ImageProcessingMessageDto.cs

--- FuncApp-TextExtractor/FuncApp-TextExtractor/Data/Dtos/ImageProcessingMessageDto.cs
+++ FuncApp-TextExtractor/FuncApp-TextExtractor/Data/Dtos/ImageProcessingMessageDto.cs
@@ -11,4 +11,6 @@ public class ImageProcessingMessageDto
     public string ImageName { get; set; } = string.Empty;
 
     public string Language { get; set; } = "en";
+
+    public string StorageLocation { get; set; } = string.Empty;
 }
